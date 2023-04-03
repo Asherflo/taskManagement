909f94b103c3929644e917e5caf8a729dc124098
Asherflo <aniyikayetemitopeflorence@gmail.com>
2023-04-03 19:29:36 +0100

    updated

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-var express = require('express');
-var mongoose = require('mongoose')
-var ejs = require("ejs");
-var bodyparser = require("body-parser"); 
-
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var app = express();
-
 app.set("view engine", "ejs");
-app.use(express.static("public"))
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost:27017/taskmanagementDB");
 
-mongoose.connect("mongodb://localhost:27017/taskmanagementDB")
-
-const itemSchema= {
-  name:String,
-
+const itemSchema = {
+  name: String,
 };
+
 const Item = mongoose.model("Item", itemSchema);
 const item1 = new Item({ name: "Welcome to ItBuddies" });
 const item2 = new Item({ name: "Like, Share and Subscribe" });
@@ -23,15 +19,40 @@ const item3 = new Item({ name: "Enjoy learning" });
 const d = [item1, item2, item3];
 
 app.get("/", function (req, res) {
-  res.render("list", { newListItem: i1 })
+  Item.find({}, function (err, f) {
+    if (f.length === 0) {
+      Item.insertMany(d, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved items to database");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { newListItem: f });
+    }
+  });
 });
 
-app.post ('/',(req,res)=>{
-    i = req.body.n;
-    i1.push(i)
-    res.redirect("/")
- })
+app.post("/", function (req, res) {
+  i = req.body.n;
+  const item = new Item({
+    name: i,
+  });
+  item.save();
+  res.redirect("/");
+});
 
-app.listen(3000,()=>{
-    console.log("listening on port 3000")
+app.post("/delete", function (req, res) {
+  Item.findByIdAndRemove(req.body.checkbox, function (err) {
+    if (!err) {
+      console.log("Successfully deleted");
+      res.redirect("/");
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("listening on port 3000.");
 });
